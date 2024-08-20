1fc15912eb6df26773182241610eb15d25dc40a4
jinx142 <kirito2005.y@gmail.com>
2024-08-20 15:56:42 +0300

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,10 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -120,6 +117,8 @@ export function renderOrderSummary() {
 
         const container = document.querySelector(`.js-cart-item-container-${productId}`)
         container.remove();
+
+        renderPaymentSummary();
       });
     });
 
@@ -129,6 +128,8 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOptionId(productId, deliveryOptionId);
         renderOrderSummary();
+
+        renderPaymentSummary();
       });
     });
 }
